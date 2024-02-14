c553360a7786e841f0642140049fee283b9b459a
juanpablotv <torresvaldesjuanpablo@gmail.com>
2024-02-14 10:06:07 -0600

    02 App Library Module and Question Models

---
+++ seani/library/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+# Create your models here.
+class Module(models.Model):
+    name = models.CharField(
+        verbose_name = "Nombre",
+        max_length=100)
+    description = models.CharField(
+        verbose_name = 'Descripción',
+        max_length = 200)
+    
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Módulo'
+        verbose_name_plural = 'Modulos'
+
+
+class Question(models.Model):
+    module = models.ForeignKey(Module, on_delete = models.CASCADE,
+                                verbose_name = 'Módulo')
+    question_text = models.CharField(
+        verbose_name = 'Texto de la Pregunta',
+        max_length=200, null = True)
+    question_image = models.ImageField(
+        verbose_name = 'Imagen de la Pregunta',
+        upload_to='questions', null = True, blank = True)
+    answer1 = models.CharField(
+        verbose_name = 'Respuesta A',
+        max_length=200)
+    answer2 = models.CharField(
+        verbose_name = 'Respuesta B',
+        max_length=200)
+    answer3 = models.CharField(
+        verbose_name = 'Respuesta C',
+        max_length=200, null = True, blank = True)
+    answer4 = models.CharField(
+        verbose_name = 'Respuesta D',
+        max_length=200, null = True, blank = True)
+    correct = models.CharField(
+        verbose_name = 'Respuesta Correcta',
+        max_length = 5)
+    
+    def __str__(self):
+        return f"{self.module} - { self.id }"
+    
+    class Meta:
+        verbose_name = 'Pregunta'
+        verbose_name_plural = 'Preguntas'
+        
+        
